b9573ad2faf7e210298798ad584660bf77d8278d
Rehab <rehabmostafa.j@gmail.com>
2023-09-23 16:40:47 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+        System.out.println("Hello I'm Rehab, best of luck!");
+
+        }
+}
